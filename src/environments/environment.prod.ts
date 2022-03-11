c60bbb02e2c5bbae30821513deedf35bed692b7f
jiayanweb <jiayanweb@gmail.com>
2022-03-10 20:41:44 -0500

    First commit.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://api.openweathermap.org/data/2.5/',
+  apiKey: '4a5c04286a35ca1f8123621a28cdbb5e'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://api.openweathermap.org/data/2.5/',
+  apiKey: '4a5c04286a35ca1f8123621a28cdbb5e'
 };
 
 /*
